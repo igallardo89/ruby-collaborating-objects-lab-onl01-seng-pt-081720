d496234347aeb7a540c07aae85012880ed317d70
Illiana Gallardo <igallardo0716@gmail.com>
2020-09-12 20:36:05 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -4,5 +4,11 @@ def MP3Importer
     def initialize(path)
      @path = path
    end
-   
+ def files
+     Dir[@path+"/*.mp3"].map { |file| file.split("/").last }
+   end
+
+    def import
+     files.each { |file| Song.new_by_filename(file) }
+   end
  end
