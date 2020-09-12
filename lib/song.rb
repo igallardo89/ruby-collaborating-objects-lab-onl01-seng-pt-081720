e78afa7c548f6410a6e8b5228ddd8ae872d8191d
Illiana Gallardo <igallardo0716@gmail.com>
2020-09-12 20:22:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,4 +18,10 @@ class Song
     new_song.artist_name = artist
     new_song
   end
+  
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end   
+  
 end 
