e09ca1184ab668845316213e9e7cb70cfdea2364
Illiana Gallardo <igallardo0716@gmail.com>
2020-09-12 20:23:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,7 +11,11 @@ class Song
     @@all
   end
   
-  def self.new_by_filename(filename)
-    
-  
+  def self.new_by_filename(file)
+     artist_name = file.split(" - ")[0]
+     song_name = file.split(" - ")[1]
+     song = Song.new(song_name)
+     song.artist_name = artist_name
+     song
+   end
 end 
