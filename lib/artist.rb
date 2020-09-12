7186482de7c17c936fe19985a1a237a6a3aec601
Illiana Gallardo <igallardo0716@gmail.com>
2020-09-12 20:04:52 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,26 @@
+class Artist 
+  attr_accessor :name, :songs 
+  
+  @@all = [ ]
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+    @songs = [ ]
+  end
+  
+  def self.all
+    @@all
+  end 
+  
+  def add_song(song)
+    self.songs << song
+  end 
+  
+  def save 
+    @@all << self
+  end
+  
+  
+  
+  end 
